ae57d8844f1ba30e84799e7a12d689afb0fb379e
Malik <astemirmail@gmail.com>
2023-05-31 12:35:36 +0600

    add Example 002 Hello User :wq :wq

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Into your name ");
+string username = Console.ReadLine();
+Console.Write("Privet, ");
+Console.Write(username);
+
