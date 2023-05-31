0f53d4c9c85f4e5fe45d108d10115973f8f9e169
Malik <astemirmail@gmail.com>
2023-05-31 17:47:09 +0600

    add Example005 If ElseConditions i

---
+++ Example005_If_Else_Conditions/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользвователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+    else
+    {
+        Console.Write("Hi, ");
+        Console.WriteLine(username);
+    }
