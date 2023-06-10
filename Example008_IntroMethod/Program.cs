bac78f94e12fe8163575ef1dcd48eb8b26f23aa2
Malik <astemirmail@gmail.com>
2023-06-10 16:33:59 +0600

    Example008_IntroMethod

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -6,6 +6,18 @@
     return result;
 }
 
+//int max = a1;
+//if(b1 > max) max = b1
+//if(c1 > max) max = c1
+
+//if(a2 > max) max = a2
+//if(b2 > max) max = b2
+//if(c2 > max) max = c2
+
+//if(a3 > max) max = a3
+//if(b3 > max) max = b3
+//if(c3 > max) max = c3
+
 
 int a1 = 15;
 int b1 = 21;
@@ -17,10 +29,11 @@ int a3 = 13;
 int b3 = 23111;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
 
+int max1 = Max(a1, b1, c1), Max(a1, b1, c1)  ;
 
 Console.WriteLine(max);
